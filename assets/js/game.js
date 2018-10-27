1ca0f478317d04c6fda7c3e35df2fb8d29e54532
danielpolk <danielpolk@mac.com>
2018-10-27 18:23:48 -0500

    added base files

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+var sport = ["basketball", "football", "soccer", "tennis", "swimming"];
+
+var sportSelect = "";
+
+function runGame() {
+    var i = Math.floor(Math.random()*sport.length);
+    sportSelect=sport[i];
+};
+
+
+startGame();
+console.log(teamSelect);
+var teamAr =teamselect.split("");
